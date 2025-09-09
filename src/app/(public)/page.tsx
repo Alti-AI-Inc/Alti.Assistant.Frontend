223f7730ee4d5397d4ef1b10d5e4f9fbc117d3a7
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-09 09:11:40 +0500

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="-mt-10 flex min-h-[calc(100vh-20px)] flex-col items-center justify-center">
       <h1 className="mb-8 text-4xl font-medium">How can I help you?</h1>
       <ChatInputComponent />
-      <p className="absolute bottom-1 text-xs text-gray-500">
+      <p className="absolute bottom-3 text-xs text-neutral-500">
         We don’t train on your data. Your chats stay private.
       </p>
     </div>
