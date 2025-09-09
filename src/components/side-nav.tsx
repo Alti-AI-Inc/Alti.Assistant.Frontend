4c6607c4d70756388cab4f40f1df1fdae9af9a23
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-09 06:09:23 +0500

    Update side-nav.tsx

--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -248,9 +248,9 @@ const SideNav = ({
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="lucide lucide-hat-glasses-icon lucide-hat-glasses"
               >
                 <path d="M14 18a2 2 0 0 0-4 0" />
