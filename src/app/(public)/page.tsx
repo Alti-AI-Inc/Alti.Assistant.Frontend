d21b2281aec74117b8ffde945a31606f90119597
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-09 09:09:04 +0500

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,6 +5,9 @@ function App() {
     <div className="-mt-10 flex min-h-[calc(100vh-20px)] flex-col items-center justify-center">
       <h1 className="mb-8 text-4xl font-medium">How can I help you?</h1>
       <ChatInputComponent />
+      <p className="absolute bottom-1 text-xs text-gray-500">
+        We don’t train on your data. Your chats stay private.
+      </p>
     </div>
   );
 }
