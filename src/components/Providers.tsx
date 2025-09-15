0c4943db6504ed22cccb4e7c98b3ebed1b870710
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-15 10:23:55 +0500

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme='light' disableTransitionOnChange>
       <SessionProvider>{children}</SessionProvider>
     </ThemeProvider>
   );
