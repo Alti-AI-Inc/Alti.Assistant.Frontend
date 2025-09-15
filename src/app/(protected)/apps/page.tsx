db48d255388eb54b0e0fa2ec36f3cae6a2a4aacc
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-15 10:19:49 +0500

    Added logos and fixed titles.

--- src/app/(protected)/apps/page.tsx
+++ src/app/(protected)/apps/page.tsx
@@ -3,15 +3,15 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { tools } from '@/lib/tools';
+import { allTools } from '@/lib/all-tools';
+import Image from 'next/image';
 import { useMemo, useState } from 'react';
 
 export default function AppIntegrationsGrid() {
   const [query, setQuery] = useState('');
 
-  
   const filteredAndSorted = useMemo(() => {
-    return tools
+    return allTools
       .filter(
         app => app.title.toLowerCase().includes(query.toLowerCase()),
         // || app.description.toLowerCase().includes(query.toLowerCase()),
@@ -28,11 +28,21 @@ export default function AppIntegrationsGrid() {
       />
       <div className="mx-auto mt-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {filteredAndSorted.map(
-          (tool: { title: string; description: string }) => (
+          (tool: { title: string; description: string; image: string }) => (
             <div key={tool.title} className="h-full">
               <Card className="h-full cursor-pointer border border-gray-200 bg-gray-100 p-0 transition-all duration-200 hover:shadow-md">
                 <CardContent className="flex flex-1 flex-col p-4">
                   <div className="flex items-center space-x-3">
+                    <div className="flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-xl bg-white transition-all duration-200 group-hover:scale-105">
+                      <Image
+                        src={tool.image}
+                        alt={tool.title}
+                        width={50}
+                        height={50}
+                        className="size-9 object-contain"
+                      />
+                    </div>
+
                     <h3 className="text-xl font-medium text-gray-900">
                       {tool.title}
                     </h3>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <SessionProvider>{children}</SessionProvider>
     </ThemeProvider>
   );
