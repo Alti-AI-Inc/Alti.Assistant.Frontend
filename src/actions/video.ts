814f68864d57766fc758754daa5a0a62bfa3d21a
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-20 00:45:40 +0500

    Make it server action

---
+++ src/actions/video.ts
@@ -0,0 +1,30 @@
+'use server';
+
+export async function getVideoUrl(operationId: string) {
+  // while (true) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/video/operations`,
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        operationId: operationId,
+      }),
+    },
+  );
+
+  const data = await response.json();
+  console.log({ data });
+
+  // Check if response exists and url does not contain storage.googleapis.com
+  const videoUrl = data?.data?.response?.videoUrl;
+  // console.log('video url', videoUrl);
+  return videoUrl;
+  // if (videoUrl.includes('https://storage.googleapis.com/')) {
+  //   return videoUrl; // ✅ exit loop & return final url
+  // }
+  // await new Promise(resolve => setTimeout(resolve, 5000));
+}
+// }
--- src/app/(protected)/c/[id]/_components/VideoComponent.tsx
+++ src/app/(protected)/c/[id]/_components/VideoComponent.tsx
@@ -1,35 +1,8 @@
 'use client';
+import { getVideoUrl } from '@/actions/video';
 import { cn } from '@/lib/utils';
 import { useEffect, useState } from 'react';
 // : Promise<string>
-async function getVideoUrl(operationId: string) {
-  // while (true) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/video/operations`,
-      {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          operationId: operationId,
-        }),
-      },
-    );
-
-    const data = await response.json();
-    console.log({ data });
-
-    // Check if response exists and url does not contain storage.googleapis.com
-    const videoUrl = data?.data?.response?.videoUrl;
-    // console.log('video url', videoUrl);
-
-    if (videoUrl.includes('https://storage.googleapis.com/')) {
-      return videoUrl; // ✅ exit loop & return final url
-    }
-    // await new Promise(resolve => setTimeout(resolve, 5000));
-  }
-// }
 
 const VideoComponent = ({ operationId }: { operationId: string }) => {
   const [loading, setLoading] = useState(true);
