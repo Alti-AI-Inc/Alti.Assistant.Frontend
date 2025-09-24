29f6dc970509807a545ce25639a123ab231fd090
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-24 17:06:26 +0500

    Show all refrences on click

--- src/app/(protected)/c/[id]/_components/ReferenceList.tsx
+++ src/app/(protected)/c/[id]/_components/ReferenceList.tsx
@@ -1,31 +1,44 @@
 // your interface file
 
+import { Button } from '@/components/ui/button';
 import { Reference } from '@/stores/useConverstionsStore';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface ReferencesProps {
   references: Reference[];
 }
 
 export default function ReferencesList({ references }: ReferencesProps) {
+  const [showFullList, setShowFullList] = useState(false);
   return (
     <div className="">
       <h1 className="mb-2 text-2xl font-bold">References</h1>
 
       <ol className="space-y-2">
-        {references.map((ref, index) => (
-          <li key={index} className="flex">
-            <span className="mr-2">{index + 1}.</span>
-            <Link
-              href={ref.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-primary font-medium underline"
-            >
-              {ref.title ? ref.title : ref.url}
-            </Link>
-          </li>
-        ))}
+        {references
+          .slice(0, showFullList ? references.length : 2)
+          .map((ref, index) => (
+            <li key={index} className="flex">
+              <span className="mr-2">{index + 1}.</span>
+              <Link
+                href={ref.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-primary font-medium underline"
+              >
+                {ref.title ? ref.title : ref.url}
+              </Link>
+            </li>
+          ))}
+
+        <Button
+          variant={'link'}
+          onClick={() => setShowFullList(!showFullList)}
+          className="text-primary  underline"
+        >
+          {showFullList ? 'Show less' : 'Show more'}
+        </Button>
       </ol>
     </div>
   );
