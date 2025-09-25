7c99e3fb7cd5076a0f9fb50329f0a408d1d531aa
Sajid Munawar <iamsajiddev@gmail.com>
2025-09-25 16:57:00 +0500

    Add loading to signout

--- src/components/modals/logout.tsx
+++ src/components/modals/logout.tsx
@@ -10,17 +10,21 @@ import {
 import { useModalStore } from '@/stores/useModalStore';
 import { signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export function Logout() {
   const { onClose, isOpen } = useModalStore();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogOut = async () => {
+    setIsLoading(true);
     await signOut({
       redirect: false,
     });
     router.push('/login');
     onClose();
+    
   };
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -38,7 +42,12 @@ export function Logout() {
             >
               Cancel
             </Button>
-            <Button onClick={handleLogOut}>Logout</Button>
+            <Button disabled={isLoading} onClick={handleLogOut}>
+              {isLoading && (
+                <span className="mr-2 h-4 w-4 animate-spin rounded-full border-b-2 border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></span>
+              )}
+              Logout
+            </Button>
           </div>
         </div>
       </DialogContent>
