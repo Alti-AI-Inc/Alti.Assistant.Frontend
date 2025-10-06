b6eea8049f134c5a1235e57e2d4398055a9b951e
Sajid Munawar <iamsajiddev@gmail.com>
2025-10-06 21:37:15 +0500

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,9 +2,10 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
-import { useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -17,9 +18,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     >
       <QueryClientProvider client={queryClient}>
         <SessionProvider>
-          {/* <AuthWatcher> */}
-            {children}
-            {/* </AuthWatcher> */}
+          <AuthWatcher>{children}</AuthWatcher>
         </SessionProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
@@ -27,38 +26,22 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   );
 }
 
-// function AuthWatcher({ children }: { children: React.ReactNode }) {
-//   const { data: session, status } = useSession();
-//   const router = useRouter();
-//   const pathname = usePathname();
-//   const publicPaths = useMemo(() => ['/login', '/register'], []);
+function AuthWatcher({ children }: { children: React.ReactNode }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const pathname = usePathname();
+  const publicPaths = useMemo(() => ['/login', '/register'], []);
 
-//   const [redirecting, setRedirecting] = useState(false);
+  useEffect(() => {
+    if (publicPaths.includes(pathname)) {
+      return;
+    }
 
-//   useEffect(() => {
-//     if (status === 'loading') return; // wait until session is resolved
+    if (session?.accessToken && session?.isTokenExpired) {
+      console.log('isTokenExpired', session?.isTokenExpired);
+      router.push('/login');
+    }
+  }, [pathname, session, status, router, publicPaths]);
 
-//     if (publicPaths.includes(pathname)) {
-//       if (session?.accessToken && !session?.isTokenExpired) {
-//         setRedirecting(true);
-//         router.push('/');
-//       }
-//       return;
-//     }
-
-//     if (!session?.accessToken || session?.isTokenExpired) {
-//       setRedirecting(true);
-//       router.push('/login');
-//     }
-//   }, [pathname, session, status, router, publicPaths]);
-
-//   if (status === 'loading' || redirecting) {
-//     return (
-//       <div className="flex h-screen items-center justify-center">
-//         <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-blue-500"></div>
-//       </div>
-//     );
-//   }
-
-//   return <>{children}</>;
-// }
+  return <>{children}</>;
+}
