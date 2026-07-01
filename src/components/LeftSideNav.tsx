ba203de27ce693c5aa9434ace37e9bfdb5280b08
Michael Meram <meram.michael@gmail.com>
2026-07-01 13:47:28 -0400

    fix(theme): left-align logo-icon fully with the left side of the search bar

--- src/components/LeftSideNav.tsx
+++ src/components/LeftSideNav.tsx
@@ -552,7 +552,7 @@ const LeftSideNav = ({ side = 'left' }: LeftSideNavProps) => {
               onClick={side === 'right' ? toggleRightSidebar : toggleLeftSidebar}
             />
           ) : (
-            <Link href="/" className="flex items-center ml-3">
+            <Link href="/" className="flex items-center">
               <Image
                 src="/assets/logo-icon.png"
                 alt="logo"
